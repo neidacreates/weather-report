74fad664441c91c083adcac3f97959b50ca8412c
Lisa <elizabethpham726@gmail.com>
2022-12-08 11:25:40 -0800

    adds flexbox items and landscape changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 'use strict';
 const currentTempElemt = document.getElementById('tempText');
+const landscapeElemt = document.getElementById('emojiScene');
 
 const state = {
   tempText: 80,
+  scene: '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂',
 };
 
+// ----- CHANGING STATES BY TEMPERATURE ----- //
+// const tempChange = (element, style1, style2, style3, style4, style5) => {
+//   element.removeAttribute('style');
+//   if (state.tempText >= 80) {
+//     element.classList.add(`${style1}`);
+//   } else if (79 >= state.tempText && state.tempText >= 70) {
+//     element.classList.add(`${style2}`);
+//   } else if (69 >= state.tempText && state.tempText >= 60) {
+//     element.classList.add(`${style3}`);
+//   } else if (59 >= state.tempText && state.tempText >= 50) {
+//     element.classList.add(`${style4}`);
+//   } else {
+//     element.classList.add(`${style5}`);
+//   }
+// };
+
+// ----- CHANGING TEMPERATURE COLOR ----- //
 const colorChange = () => {
   currentTempElemt.removeAttribute('style');
   if (state.tempText >= 80) {
@@ -20,27 +39,47 @@ const colorChange = () => {
   }
 };
 
+// ----- CHANGING TEMPERATURE COUNT ----- //
 const increaseTemp = () => {
   state.tempText += 1;
   colorChange();
+  landscapeChange();
   currentTempElemt.textContent = `${state.tempText}`;
 };
-
-tempText.addEventListener('increaseTemp', increaseTemp);
 
 const decreaseTemp = () => {
   state.tempText -= 1;
   colorChange();
+  landscapeChange();
   currentTempElemt.textContent = `${state.tempText}`;
 };
 
-tempText.addEventListener('decreaseTemp', decreaseTemp);
+// ----- CHANGING LANDSCAPE SCENE ----- //
 
+const landscapeChange = () => {
+  if (state.tempText >= 80) {
+    state.scene = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (79 >= state.tempText && state.tempText >= 70) {
+    state.scene = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (69 >= state.tempText && state.tempText >= 60) {
+    state.scene = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (59 >= state.tempText && state.tempText >= 50) {
+    state.scene = '💨🍃🌬️💨🍃🌬️💨🍃🌬️💨🍃🌬️';
+  } else {
+    state.scene = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  }
+
+  landscapeElemt.textContent = state.scene;
+};
+
+// ----- REGISTERING EVENT HANDLERS ----- //
 const registerEventHandlers = () => {
   const increaseTempButton = document.getElementById('tempIncrease');
   increaseTempButton.addEventListener('click', increaseTemp);
+
   const decreaseTempButton = document.getElementById('tempDecrease');
   decreaseTempButton.addEventListener('click', decreaseTemp);
 };
 
+// ----- SETTING SO DOM LOADS BEFORE JS ----- //
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
